a61bbe1a304db1efc86fd70a7d35b2a0c282aa4c
Celeste Lavoie <celeste.lavoie@gmail.com>
2016-03-11 09:59:37 -0500

    stuff

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 var $header = $('header');
 var $h1 = $('h1');
 var $main = $('main');
+var $p1 = $('main p:first-child');
+var $h2 = $('h2');
+var $li = $('li');
+var $img = $('img');
 
-$header.addClass('.masthead');
-$h1x.removeClass('.big-heading');
-$main.addClass('.wrapper');
+var $ul = $('ul li:first-child');
+
+var $dl = $('dl');
+var $dt = $('dt');
+
+$header.addClass('masthead');
+$h1.removeClass('big-heading');
+$main.addClass('wrapper');
+$p1.addClass('intro');
+$h2.addClass('slug-head');
+$li.addClass('slug-list');
+$img.addClass('slug-img');
+
+$dl.addClass('classification');
+$dt.removeClass('big-label');
